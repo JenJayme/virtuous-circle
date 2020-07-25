3018a0fa8841daa3b19e52b723570b49e2c79be1
Jen Jayme <jeniferjayme@gmail.com>
2020-07-25 16:28:05 -0700

    Added updateOne() and module.exports to orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,5 @@
 var connection = require('./connection.js');
+const { query } = require('express');
 
 var orm = {
     selectAll: function selectAll(table) {
@@ -9,7 +10,7 @@ var orm = {
         });
     },
 
-    insertOne: function insertOne(table, fields, values) {
+    insertOne: function insertOne(table, fields, values, cb) {
         var queryString = 'INSERT INTO '+ table;
         queryString += ' (';
         queryString += fields.toString();
@@ -18,55 +19,52 @@ var orm = {
 
         connection.query(queryString, [table, fields, values], function(err, results) {
             if (err) throw err;
-            console.log(results);
+
+            CB(results);
         });
     },
-    
 
+    updateOne: function updateOne(table, condition, updateFieldValObj, cb) {
+        var queryString = 'UPDATE ' + table;
+        queryString += ' SET ';
+        queryString += objToSql(updateFieldValObj);
+        queryString += ' WHERE ';
+        queryString += condition;
 
+        console.log('queryString =' + queryString);
+        connection.query(queryString, function (err, results) {
+            if (err) { 
+                throw err
+            }
+            cb(results);
+        });
+    }
 }
 
 
 //=============================================================
 
-//BORROWED FOR REFERENCE - delete
-
-var connection = require("./connection.js");
-const { query } = require('express');
-
-var orm = {
-  select: function(whatToSelect, tableInput) {
-    var queryString = "SELECT ?? FROM ??";
-    connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
-      if (err) throw err;
-      console.log(result);
-    });
-  },
-  selectWhere: function(tableInput, colToSearch, valOfCol) {
-    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-
-    console.log(queryString);
-
-    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
-      if (err) throw err;
-      console.log(result);
-    });
-  },
-  leftJoin: function(whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol) {
-    var queryString = "SELECT ?? FROM ?? AS tOne";
-    queryString += " LEFT JOIN ?? AS tTwo";
-    queryString += " ON tOne.?? = tTwo.??";
-
-    console.log(queryString);
-
-    connection.query(queryString, [whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol], function(
-      err,
-      result
-    ) {
-      if (err) throw err;
-      console.log(result);
-    });
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
   }
-};
 
 module.exports = orm;
