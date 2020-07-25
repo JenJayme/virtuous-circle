e62aa6b8c4c6e82e980c91e46005c2d7d5ad032f
Jen Jayme <jeniferjayme@gmail.com>
2020-07-25 15:00:31 -0700

    Added connection details to config\connection.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,16 @@
 INSERT INTO foodbundles (bundle_name, quantity, claimed)
 VALUES ('beans', '20 cans','1'),('apples', '2 crates', '0'),('strawberries','3 crates','0'),('trout','5 large','0');
+
+
+-- To Run schema.sql and seeds.sql from command line: 
+
+-- Navigate in explorer to this folder where MySQL program resides:
+-- C:\Program Files\MySQL\MySQL Server 8.0\bin
+
+-- Open Windows Powershell as Administrator here
+
+-- run these commands: 
+
+-- source \Users\floje\OneDrive\Desktop\codecamp\homework\virtuous-circle\db\schema.sql
+
+-- source \Users\floje\OneDrive\Desktop\codecamp\homework\virtuous-circle\db\seeds.sql
