80ded9c9f98e6af8e06204f94ae2f17499a0c13c
Jen Jayme <jeniferjayme@gmail.com>
2020-07-25 22:40:56 -0700

    Built out server.js

--- server.js
+++ server.js
@@ -1,2 +1,30 @@
 var express = require('express');
 
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the public directory
+app.use(express.static('public'));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Use Handlebars
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs ({ defaultLayout: 'main' }));
+
+app.set('view engine', 'handlebars');
+
+// Import routes and give server access to them
+var routes = require('./controllers/controller.js');
+
+app.use(routes);
+
+// Start the server so that it begins listening to client requests.
+app.listen(PORT, function() {
+    //log when the server has started
+    console.log('Server listening on http://localhost:' + PORT);
+});
