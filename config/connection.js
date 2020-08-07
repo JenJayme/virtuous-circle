6fb87f7d4812178bf3a0b6f21b15e2042111b250
Jen Jayme <jeniferjayme@gmail.com>
2020-08-06 20:50:03 -0700

    Switched connection details to Heroku for new push.

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,16 @@
 var mysql = require("mysql");
 
 // for local testing
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "virtuous_circle_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "virtuous_circle_db"
+// });
 
 // for Heroku
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 // Connect to the database
 connection.connect(function(err) {
